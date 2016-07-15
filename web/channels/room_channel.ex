5874e1c0859a90d36c8508704743c13f60a3a77a
Helapu <foolway@126.com>
2016-07-15 11:45:01 +0800

    filter msgs

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,15 +2,29 @@ defmodule GdPush.RoomChannel do
   use Phoenix.Channel
   require Logger
 
-  def join("rooms:boom", msg, socket) do
-    Logger.debug "room channel join #{msg["user"]}"
-    if msg["user"] == "helapu" do
-      send(self, :after_join)
-      {:ok, socket}
-    else
-      {:error , %{reason: "用户名错误"}}
-    end
+  intercept ["new:msg"]
 
+  def join("rooms:plan", payload, socket) do
+    # 验证账户
+    Logger.debug "join uuid: #{payload["uuid"]}, token: #{payload["token"]}"
+    Logger.debug "join others: #{payload["body"]} #{payload["subject"]}"
+    Logger.debug "ios: #{payload["message"]} #{payload["subject"]}"
+
+    :timer.send_interval(5000, :ping)
+
+    socket = assign(socket, :uuid, payload["uuid"])
+    socket = assign(socket, :token, payload["token"])
+
+    send(self, :after_join)
+    {:ok, socket}
+
+    # if true do
+    #   send(self, :after_join)
+    #   {:ok, socket}
+    # else
+    #   push socket, "error", %{error: "哎呀，你的用户名或者密码错啦"}
+    #   {:error , %{reason: "用户名错误"}}
+    # end
   end
 
   def join("rooms:" <> _private_room_id, _params, _socket) do
@@ -19,13 +33,18 @@ defmodule GdPush.RoomChannel do
 
   def handle_info(:after_join, socket) do
     Logger.debug "room channel after join"
-    push socket, "join", %{status: "connected"}
-    push socket, "new:msg", %{user: "SYSTEM", body: "ping"}
+    push socket, "join", %{token: "uoeu--oeuseousuh4390heuoeubuoeueo"} # 这里可以返回udid登陆令牌
+    push socket, "system", %{user: "SYSTEM", body: "ping"}
 
     {:ok, conn} = Redix.start_link()
     {:ok, cnt} = Redix.command(conn, ~w(INCR WebSocket))
-    broadcast! socket, "count:online", %{online: cnt}
+    broadcast! socket, "count:online", %{online: cnt} # 通知所有连接，更新在线数
 
+    {:noreply, socket}
+  end
+
+  def handle_info(:ping, socket) do
+    push socket, "heartbeat", %{fromuuid: "SYSTEM", body: "ping"}
     {:noreply, socket}
   end
 
@@ -38,17 +57,39 @@ defmodule GdPush.RoomChannel do
     :ok
   end
 
-  def handle_in("new:msg", msg, socket) do
-    # Logger.debug "收到客户端的信息: #{msg["user"]} -- #{msg["body"]}"
-    # System.cmd "say", ["收到 #{msg["user"]} 的消息 #{msg["body"]} "]
-
-    broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"]}
-    {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])}
+  def handle_in("new:msg", payload, socket) do
+    Logger.debug "收到客户端的信息: #{payload["fromuuid"]} -- #{payload["touuid"]} -- #{payload["msg"]}"
+    broadcast! socket, "new:msg", payload
+    {:reply, {:ok,  payload }, socket}
   end
 
+
+  def handle_in("heartbeat", payload, socket) do
+    # 回应客户端，服务器还在干活滴
+    push socket, "heartbeat", %{user: "SYSTEM"}
+    {:reply, {:ok,  payload }, socket}
+  end
+
+
   def handle_out("new:msg", payload, socket) do
-    push socket, "new:msg", payload
-    {:noreply, socket}
+    from = payload["fromuuid"]
+    uid = socket.assigns[:uuid]
+    touser = payload["touuid"]
+    msg = payload["msg"]
+
+    Logger.debug "from: #{from}, uid: #{uid},  touser: #{touser}, msg: #{msg}"
+
+    if from == "SYSTEM" do
+      push socket, "heartbeat", payload
+      {:noreply, socket}
+    end
+
+    if uid == touser or from == uid do
+      push socket, "new:msg", payload
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
   end
 
 end
--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -1,9 +1,20 @@
 import {Socket, LongPoller} from "phoenix"
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
-class InnerHello {
+class WebChat {
 
-  static init(){
+  static init() {
+    let socket = new Socket("/socket", {params: {token: window.userToken}})
+
+    console.log("socket connecting")
+    socket.connect()
+
+    $('#loginbtn').click( function () {
+      console.log("login btn");
+      WebChat.connect()
+    })
+  }
+  static connect(){
 
     let socket = new Socket("/socket", {params: {token: window.userToken}})
 
@@ -14,13 +25,15 @@ class InnerHello {
     var $messages  = $("#messages")
     var $input     = $("#message-input")
     var $username  = $("#username")
+    var $tousername  = $("#tousername")
+
     var $onlinecount  = $("#onlinecount")
 
     socket.onOpen( ev => console.log("OPEN", ev) )
     socket.onError( ev => console.log("ERROR", ev) )
     socket.onClose( e => console.log("CLOSE", e))
 
-    var chan = socket.channel("rooms:boom", {user: "helapu", pwd: "4242"})
+    var chan = socket.channel("rooms:plan", {uuid: $username.val(), token: "4242"})
     chan.join()
       .receive("ok", resp => { console.log("Joined successfully", resp) })
       .receive("error", resp => { console.log("Unable to join", resp) })
@@ -28,18 +41,9 @@ class InnerHello {
     chan.onError(e => console.log("something went wrong", e))
     chan.onClose(e => console.log("channel closed", e))
 
-    $username.val( Cookies.get("username") )
-    console.log( Cookies.get("username") );
-
-    $username.change(function() {
-      console.log($(this).val());
-      Cookies.set("username", "helapu");
-    })
-
-
     $input.off("keypress").on("keypress", e => {
       if (e.keyCode == 13) {
-        chan.push("new:msg", {user: $username.val(), body: $input.val()})
+        chan.push("new:msg", {fromuuid: $username.val(), touuid: $tousername.val(), msg: $input.val()})
         $input.val("")
       }
     })
@@ -55,7 +59,7 @@ class InnerHello {
     })
 
     chan.on("user:entered", msg => {
-      var username = this.sanitize(msg.user || "anonymous")
+      var username = this.sanitize(msg.fromuuid || "anonymous")
       $messages.append(`<br/><i>[${username} entered]</i>`)
     })
   }
@@ -63,12 +67,12 @@ class InnerHello {
   static sanitize(html){ return $("<div/>").text(html).html() }
 
   static messageTemplate(msg){
-    let username = this.sanitize(msg.user || "anonymous")
-    let body     = this.sanitize(msg.body)
+    let username = this.sanitize(msg.fromuuid || "anonymous")
+    let info     = this.sanitize(msg.msg)
 
-    return(`<p><a href='#'>[${username}]</a>&nbsp; ${body}</p>`)
+    return(`<p><a href='#'>[${username}]</a>&nbsp; ${info}</p>`)
   }
 
 }
 
-InnerHello.init()
+WebChat.init()
